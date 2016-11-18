7a40cda6ca8e542cbefd696f4d06e2728fc06d37
Michael Knoch <info@michaelknoch.de>
2016-11-18 11:24:05 +0100

    implement visibility button

--- imports/ui/components/createTask/index.js
+++ imports/ui/components/createTask/index.js
@@ -1,16 +1,33 @@
 import { Template } from 'meteor/templating';
 import { Todos } from '/imports/api/todos';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './createTask.html';
 import './createTask.less';
 
+
+Template.createTask.created = function () {
+    this.publicActive = new ReactiveVar(false);
+};
+
+Template.createTask.helpers({
+    getSecurity: function() {
+        return Template.instance().publicActive.get() ? 'public' : 'private';
+    }
+});
+
 Template.createTask.events({
+
+    'click .visibility-trigger-box': (event, template) => {
+        template.publicActive.set(!template.publicActive.get());
+    },
+
     'submit .new-task': (event) => {
         event.preventDefault();
         console.info('new stuff arrived');
 
         const text = event.target.text.value;
-        const _public = event.target.security.checked;
+        const _public = Template.instance().publicActive.get();
 
         if (text.length < 1) {
             return;
