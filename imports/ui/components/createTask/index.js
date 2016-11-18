b63eb7120cb1222b35cfee43f382df47a25dcc87
Michael Knoch <info@michaelknoch.de>
2016-11-18 11:43:54 +0100

    remove debugger, refactor everything

--- imports/ui/components/createTask/index.js
+++ imports/ui/components/createTask/index.js
@@ -6,11 +6,25 @@ import './createTask.html';
 import './createTask.less';
 
 
-Template.createTask.created = function () {
+Template.createTask.created = function() {
+
+    // this points to Template.instance()
+    // component args can be accessed with this.data
+
+    if (Template.instance() === this) {
+        console.info('ist echt so');
+    } else {
+        console.error('ist nicht so');
+    }
+
     this.publicActive = new ReactiveVar(false);
 };
 
 Template.createTask.helpers({
+
+    // this points to the data/args of the component instantiation
+    //access `real this/Template instance` with Template.instance()
+
     getSecurity: function() {
         return Template.instance().publicActive.get() ? 'public' : 'private';
     }
@@ -18,16 +32,19 @@ Template.createTask.helpers({
 
 Template.createTask.events({
 
+    // to access template context from events, use Template.instance()
+    // or the event parameter in event invocation
+
     'click .visibility-trigger-box': (event, template) => {
         template.publicActive.set(!template.publicActive.get());
     },
 
-    'submit .new-task': (event) => {
+    'submit .new-task': (event, template) => {
         event.preventDefault();
         console.info('new stuff arrived');
 
         const text = event.target.text.value;
-        const _public = Template.instance().publicActive.get();
+        const _public = template.publicActive.get();
 
         if (text.length < 1) {
             return;
